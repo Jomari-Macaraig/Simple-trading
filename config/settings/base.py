f3b283e6e39ffe9105e242d2686c9c80575ad184
Jomari Macaraig <jomari@auxahelth.com>
2024-09-15 19:06:50 +0800

    Made API Authenticated

--- config/settings/base.py
+++ config/settings/base.py
@@ -151,6 +151,13 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+    "PAGE_SIZE": 10
 }
